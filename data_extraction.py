eb12d9db4d87134c36ae15e4bbe2b98b0755f8fd
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2023-11-08 21:18:23 +0100

    demo for downloading wiki pages and adding users

--- data_extraction.py
+++ data_extraction.py
@@ -27,15 +27,39 @@ def get_random_id(max_ids=10**15):
     return random.randint(0, max_ids)
 
 
-class WebScrapingPipeline:
+def request_to_wiki(title):
+    """ return response from wikipedia api given some url and title """
+    params = {'action': 'query',
+              'format': 'json',
+              'titles': title,
+              'prop': 'extracts',
+              'exintro': True,
+              'explaintext': True}
+    return requests.get(WIKI_URL, params=params).json()
 
+
+def request_text_from_wiki(title):
+    """ extract text from wikipedia page given some title """
+    pages = request_to_wiki(title)['query']['pages']
+    k = list(pages.keys())[0]
+    try:
+        return pages[k]['extract']
+    except KeyError:
+        raise ValueError(f'page "{title}" not found')
+
+
+class WebScrapingPipeline:
+    """
+    pipeline for building a dataset of bags of words from wikipedia pages
+    """
     def __init__(self, bag_creator):
         self.bag_creator = bag_creator
+        self.data_dir_path = 'data'
         self._build_env()
 
     def _build_env(self):
         # build environment
-        root = self.get_data_dir_path()
+        root = self.data_dir_path
         paths = [root, root + '/user_data']
 
         # create missing directories
@@ -48,28 +72,13 @@ class WebScrapingPipeline:
             with open(self.get_wiki_bags_path(), 'wb') as f:
                 pickle.dump(dict(), f)
 
-    def get_data_dir_path(self):
-        return 'data'
-
     def get_wiki_bags_path(self):
-        return self.get_data_dir_path() + '/wiki_bags.pkl'
+        """return the path where the wiki bag of words are saved"""
+        return self.data_dir_path + '/wiki_bags.pkl'
 
     def get_user_data_path(self, user_id):
-        return self.get_data_dir_path() + f'/user_data/user_{user_id}.pkl'
-
-    def request_to_wiki(self, title):
-        """ return response from wikipedia api given some url and title """
-        params = {'action': 'query',
-                  'format': 'json',
-                  'titles': title,
-                  'prop': 'extracts',
-                  'exintro': True,
-                  'explaintext': True}
-        return requests.get(WIKI_URL, params=params).json()
-
-    def request_text_from_wiki(self, title):
-        """ extract text from wikipedia page given some title """
-        return self.request_to_wiki(title)['query']['pages']['23862']['extract']
+        """return the path where the user data is saved"""
+        return self.data_dir_path + f'/user_data/user_{user_id}.pkl'
 
     def convert_text_to_bag(self, text: str):
         """return bag of words given some text"""
@@ -84,8 +93,14 @@ class WebScrapingPipeline:
         with open(self.get_wiki_bags_path(), 'wb') as f:
             pickle.dump(wiki_bags, f)
 
+    def add_bag(self, title):
+        """add bag to the dictionary saved in the file"""
+        text = request_text_from_wiki(title)
+        bag = self.convert_text_to_bag(text)
+        self.save_bag(title, bag)
+
     def add_user(self):
-        # create user_data/user_{user_id}.pkl if it doesn't exist
+        """add user to the database and return his id"""
         user_id = get_random_id()
         path = self.get_user_data_path(user_id)
         if not os.path.exists(path):
@@ -94,13 +109,21 @@ class WebScrapingPipeline:
         return user_id
 
     def get_bags(self) -> dict:
+        """return dictionary of all the bags in the database"""
         with open(self.get_wiki_bags_path(), 'rb') as f:
             return pickle.load(f)
 
 
-def test_1(title='Python (programming language)'):
-    """ test request_text_from_wiki """
-    words = ['python', 'programming', 'gigachad', 'meme']
+def test_1():
+    """
+    test use of pipeline:
+    - create new user
+    - download some bags
+    - show results
+    """
+
+    # chose bag of wards
+    words = ['python', 'programming', 'matrix', 'batteries', 'budget', 'engineering', 'science']
 
     def bag_creator(text):
         return [word in text for word in words]
@@ -108,26 +131,21 @@ def test_1(title='Python (programming language)'):
     # initialize pipeline
     pipeline = WebScrapingPipeline(bag_creator)
 
+    # dd user (just for testing)
     new_user_id = pipeline.add_user()
     print('\nOwO! Welcome to the party, user', new_user_id)
 
-    # get text
-    text = pipeline.request_text_from_wiki(title)
+    # download this titles
+    titles = ['Python (programming language)', 'The Matrix', 'Linear algebra']
+    for title in titles:
+        pipeline.add_bag(title)
 
-    # get and save bag
-    bag = pipeline.convert_text_to_bag(text)
-    pipeline.save_bag(title, bag)
-
-    print('\n')
-    print(f'text {text}')
-    print(f'words {words}')
-    print(f'bag = {bag}')
-
-    print('\n')
-    print('all bags:')
+    # show the results
+    print(f'\nwords = {words}')
+    print('\nall bags:')
     bags = pipeline.get_bags()
     for title in bags:
-        print(f'{title} {bags[title]}')
+        print(f'{bags[title]} {title}')
 
 
 if __name__ == '__main__':
