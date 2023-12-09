0935a1da835b90a122e7a7283211d04fe38568d5
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2023-12-09 21:15:11 +0100

    first upload

---
+++ data_engineering/wiki_database.py
@@ -0,0 +1,36 @@
+"""
+This class is used to interact with the database of all the Wikipedia pages
+
+--------------------
+Useful methods:
+- get_random_pages: select a random file of the database, then get a list of random titles from that file
+- get_training_data: given a list of titles, get a dataframe with the corresponding binary arrays
+"""
+import pandas as pd
+import numpy as np
+import os
+
+
+class WikiDatabase:
+
+    def __init__(self, dir_path):
+        self.dir_path = dir_path
+        self.file_names = [s for s in os.listdir(dir_path) if s.endswith('.parquet')]
+        self.n_files = len(self.file_names)
+
+    def get_random_pages(self, n_pages):
+        """select a random file of the database, then get a list of random titles from that file"""
+        file_name = self.file_names[np.random.randint(self.n_files)]
+        df = pd.read_parquet(os.path.join(self.dir_path, file_name))
+        return df.sample(n_pages).TITLE.values.tolist()
+
+    def get_training_data(self, titles):
+        """given a list of titles, get a dataframe with the corresponding binary arrays"""
+        data = pd.DataFrame()
+        data = data.reset_index(drop=True)
+        for file_name in self.file_names:
+            df = pd.read_parquet(os.path.join(self.dir_path, file_name))
+            df = df[df.TITLE.isin(titles)]
+            df = df.reset_index(drop=True)
+            data = pd.concat([data, df])
+        return data
---
+++ data_engineering/wiki_database_test.py
@@ -0,0 +1,29 @@
+from wiki_database import WikiDatabase
+
+
+def test_get_random_pages():
+
+    # initialize the database
+    wiki_database = WikiDatabase('data\\wiki_database')
+
+    # get 10 random titles
+    titles = wiki_database.get_random_pages(10)
+    print(titles)
+
+
+def test_get_training_data():
+
+    # initialize the database
+    wiki_database = WikiDatabase('data\\wiki_database')
+
+    # get your titles (here I choose at random)
+    titles = wiki_database.get_random_pages(10)
+
+    # get the training data
+    data = wiki_database.get_training_data(titles)
+    print(data)
+
+
+if __name__ == '__main__':
+    test_get_random_pages()
+    # test_get_training_data()
